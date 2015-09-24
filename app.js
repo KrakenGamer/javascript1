3149250bebfef8cc0ba6ea1a238b22438209c4c3
KrakenGamer <franklintokioka@student.midpac.edu>
2015-09-23 15:42:46 -1000

    Hello Everybody!
    This is a compilation of weird stuff. Enjoy!

---
+++ app.js
@@ -0,0 +1,8 @@
+var press = function() {
+	alert("What the heck is a sonic!? PINGAS CHIPS ARE nice! (this is my first project so sorry if it is low quality)");
+};
+
+var ask = function() {
+	var name= prompt("What the heck is a sonic!?");
+	alert("Pingas" + name + "Pingas");
+};
