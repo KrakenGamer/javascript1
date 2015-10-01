3eefd120a7a3ff69a8e926a8c33f247a54dc092f
KrakenGamer <franklintokioka@student.midpac.edu>
2015-09-30 16:54:10 -1000

    Press that button!
    Can you click the button fast enough? cooler now will not like it if
    you are bad at this.

--- app.js
+++ app.js
@@ -5,9 +5,24 @@ var highscore=0;
 var press= function() {
 	Score++;
 	document.getElementById('Score').innerHTML = "Your awesome score" +Score;
+	if (Score === 19){
+	alert("Press the button as much as you can. If you don't then coolernow will come after you.")
+	timerbutton();
+	setTimeout(removeButton, 5000);
+}
 	if (Score > highscore) {
 		document.getElementById('highscore').innerHTML = "New Highscore" + Score;
 		return;
-
+		setInterval(Timer2, 1000);
 	}
+
+
 };
+
+
+
+
+function Timer2() {
+    var d = new Date();
+    document.getElementById("demo").innerHTML = d.toLocaleTimeString();
+}
