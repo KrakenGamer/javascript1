1bf4426704ef70157fff2c1284b91e48adb968db
KrakenGamer <franklintokioka@student.midpac.edu>
2015-10-07 16:24:36 -1000

    debugging and changes
    Just fixing up stuff. It may not be interesting, but it fixes my
    project!

--- app.js
+++ app.js
@@ -1,28 +1,26 @@
+var gamebutton = function(){
+
+	document.getElementById('button').innerHTML = '<input onclick="press()" type="image" id="button" src="saosplatoon.jpg">'
+} 
+
+
 var Score=0;
 
-var highscore=0;
+var highscore=19;
 
-var press= function() {
+var press = function() {
 	Score++;
-	document.getElementById('Score').innerHTML = "Your awesome score" +Score;
+	document.getElementById('Score').innerHTML = "Your awesome score " + Score;
 	if (Score === 19){
-	alert("Press the button as much as you can. If you don't then coolernow will come after you.")
-	timerbutton();
-	setTimeout(removeButton, 5000);
-}
-	if (Score > highscore) {
-		document.getElementById('highscore').innerHTML = "New Highscore" + Score;
-		return;
-		setInterval(Timer2, 1000);
+		alert("Press the button as much as you can. If you don't then coolernow will come after you.");
+		setInterval(gameover, 5000);
 	}
-
-
+	else if (Score > highscore) {
+		document.getElementById('highscore').innerHTML = "New Highscore " + Score;
+		return;
+	}
 };
 
-
-
-
-function Timer2() {
-    var d = new Date();
-    document.getElementById("demo").innerHTML = d.toLocaleTimeString();
+function gameover() {
+	document.getElementById('button').innerHTML = '';
 }
