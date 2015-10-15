47b6c00d964d8977c7ebe53ac7d7af5811a767dd
KrakenGamer <franklintokioka@student.midpac.edu>
2015-10-14 16:41:48 -1000

    pro updates
    more updates!

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var gamebutton = function(){
-
-	document.getElementById('button').innerHTML = '<input onclick="press()" type="image" id="button" src="saosplatoon.jpg">'
+	document.getElementById('button').innerHTML = '<input onclick="press()" type="image" id="button" src="Untitled.png">'
 } 
 
 
@@ -10,10 +9,13 @@ var highscore=19;
 
 var press = function() {
 	Score++;
-	document.getElementById('Score').innerHTML = "Your awesome score " + Score;
-	if (Score === 19){
+	document.getElementById('Score').innerHTML = "Your awesome score: " + Score;
+	if (Score === 21){
 		alert("Press the button as much as you can. If you don't then coolernow will come after you.");
 		setInterval(gameover, 5000);
+		script>
+
+    
 	}
 	else if (Score > highscore) {
 		document.getElementById('highscore').innerHTML = "New Highscore " + Score;
