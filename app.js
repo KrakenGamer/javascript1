ffbeed4c55fe4e48f884a547653740bde5c158da
coolernow <coolernow641@gmail.com>
2015-10-28 16:37:18 -1000

    moar mama jokes!

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var yomama = new Array("Yo Mama so short, she can hang glide on doritos, Yo Mama so fat when you kill her in Call of Duty you get a 5 killstreak, Yo Mama so short she can't even get high, Yo Mama so fat when she goes into sace there is no space,")
+var yomama = new Array("Yo Mama so short, she can hang glide on doritos, Yo Mama so fat when you kill her in Call of Duty you get a 5 killstreak, Yo Mama so short she can't even get high, Yo Mama so fat when she goes into sace there is no space, Yo mama so stupid she stays up all night trying to catch some sleep, Yo mama so fat when she goes to safeway it isn't safe anymore, Yo mama so ugly she made by happy meal cry,")
 // this is a yo mama joke compilation //
 var select = [Math.floor(Math.random() * 3)];
 document.getElementById("cookie").innerHTML = yomama[thing];
